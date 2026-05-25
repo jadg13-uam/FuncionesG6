e131f8bc4fc21afc8d2b890788e333db00fb2674
Jamesjsfo <jsfernandez@uamv.edu.ni>
2026-05-25 12:35:38 -0600

    feat: suma

--- Funcion1/Program.cs
+++ Funcion1/Program.cs
@@ -1,8 +1,14 @@
 ﻿//Crear un menu de opciones para el usuario, donde pueda elegir entre las siguientes opciones:
 //1. Sumar dos numeros
+
 //2. Restar dos numeros
 //3. Multiplicar dos numeros
 //4. Dividir dos numeros
 //5. Salir
 
 int num1, num2, resultado, opcion;
+
+int sumar(int num1, int num2)
+{
+    return num1 + num2;
+}
