f18eec9ab483275b955458a8114e17887a014282
Duran <jose.duran@uamv.edu.ni>
2026-05-25 12:05:58 -0600

    feat: Configuración de app

---
+++ Funcion1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Crear un menu de opciones para el usuario, donde pueda elegir entre las siguientes opciones:
+//1. Sumar dos numeros
+//2. Restar dos numeros
+//3. Multiplicar dos numeros
+//4. Dividir dos numeros
+//5. Salir
+
+int num1, num2, resultado, opcion;
